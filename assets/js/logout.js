95eece742d4a7a2e93a10bebf9d92a5bdb2255c0
Aleksei Berdnik <avberdnik@gmail.com>
2025-11-23 18:48:14 +0100

    Delete assets directory

--- assets/js/logout.js
+++
@@ -1,9 +0,0 @@
-import { logout } from './auth.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const btn = document.getElementById('logout-btn');
-  btn?.addEventListener('click', (e) => {
-    e.preventDefault();
-    logout();
-  });
-});
